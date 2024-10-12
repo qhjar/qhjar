3a77afd50aa9051559123feb82a21dcfc989ba26
Arrow <89811520+ArrowAced@users.noreply.github.com>
2024-10-11 22:26:38 -0400

    qhjar?

---
+++ qhjar.js
@@ -0,0 +1,49 @@
+const txtenc = new TextEncoder();
+const txtdec = new TextDecoder();
+export const encodeQhjar = function(data) {
+  let binary = "";
+
+  for (const byte of data) {
+    binary += byte.toString(2).padStart(8,"0");
+  }
+
+  let result = "";
+
+  while (binary.length >= 4) {
+    if (binary.length >= 12) {
+      let chunk = binary.slice(0,12);
+      binary = binary.slice(12);
+      result += String.fromCodePoint(0xE0000 + parseInt(chunk,2));
+    } else {
+      let chunk = binary.slice(0,4);
+      binary = binary.slice(4);
+      result += String.fromCodePoint(0xFE00 + parseInt(chunk,2));
+    }
+  }
+  return result;
+}
+export const decodeQhjar = function(str) {
+  let binary = "";
+
+  for (const char of str) {
+    const codepoint = char.codePointAt(0);
+    if (codepoint >= 0xE0000 && codepoint <= 0xE0FFF) {
+      const value = codepoint - 0xE0000;
+      binary += value.toString(2).padStart(12,"0");
+    }
+    if (codepoint >= 0xFE00 && codepoint <= 0xFE0F) {
+      const value = codepoint - 0xFE00;
+      binary += value.toString(2).padStart(4,"0");
+    }
+  }
+
+  let data = [];
+
+  while (binary.length >= 8) {
+    data.push(parseInt(binary.slice(0,8),2));
+    binary = binary.slice(8);
+  }
+  return new Uint8Array(data);
+}
+export const encodeQhjarStr = function(str) {return encodeQhjar(txtenc.encode(str))};
+export const decodeQhjarStr = function(str) {return txtdec.decode(decodeQhjar(str))};
